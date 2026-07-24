fb53f1ecafe5309fed6bec699f6f0d195ba74353
evann-pommier <evannpommier64@gmail.com>
2026-07-24 13:15:12 +0200

    Ajout Spring Security sur patient-service, dateOfBirth en LocalDate, fix config gateway

--- patient/src/main/java/com/openclassrooms/MediLaboSolutions/patient/model/Patient.java
+++ patient/src/main/java/com/openclassrooms/MediLaboSolutions/patient/model/Patient.java
@@ -2,7 +2,11 @@ package com.openclassrooms.MediLaboSolutions.patient.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "patients")
@@ -12,30 +16,24 @@ import lombok.*;
 @AllArgsConstructor
 public class Patient {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @NotBlank(message = "Le prénom est obligatoire")
     private String firstName;
-
 
     @NotBlank(message = "Le nom est obligatoire")
     private String lastName;
 
-
-    @NotBlank(message = "La date de naissance est obligatoire")
-    private String dateOfBirth;
-
+    @NotNull(message = "La date de naissance est obligatoire")
+    @Past(message = "La date de naissance doit être dans le passé")
+    private LocalDate dateOfBirth;
 
     @NotBlank(message = "Le genre est obligatoire")
     private String gender;
 
-
     private String address;
-
 
     private String phone;
 
